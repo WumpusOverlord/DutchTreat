c70a1754718c7b7d7d95046e02e10281488703e7
Hugo <Hugo@OX-Admins-MacBook-Pro.local>
2018-01-05 09:47:33 +0000

    First commit of Dutch Treat

---
+++ DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+
+     
+        public DutchContext(DbContextOptions<DutchContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        
+    }
+}
